d91da97f168f825e84565e37056a16165481e483
chien10032005-afk <chien10032005@gmail.com>
2025-10-02 13:15:58 +0700

    First commit - Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: NetworkImage("https://i.pravatar.cc/150?img=3"),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "Trung Chien",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Ho Chi Minh, Viet Nam",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+D
